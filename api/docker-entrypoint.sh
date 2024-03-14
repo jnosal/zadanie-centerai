55f1869185a5d624908715468c8c6e03274d52c3
Jacek Nosal <jacek.nosal@outlook.com>
2024-03-14 17:16:18 +0100

    Cleanup settings, add ret framework camelcase

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -5,5 +5,6 @@ while ! nc -z database 5432; do
   sleep 0.1
 done
 echo "PostgreSQL started"
+python3.11 manage.py migrate --settings=zadanie.settings.dev
 python3.11 manage.py collectstatic --noinput --settings=zadanie.settings.dev
 python3.11 manage.py runserver 0.0.0.0:4444 --settings=zadanie.settings.dev
